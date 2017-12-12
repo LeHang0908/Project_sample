e0f0ef5c5c5c3e52ddefae50f0eae509334cc07f
LeHang0908 <lehanghd96@gmail.com>
2017-12-12 09:04:58 +0700

    finish advanced login

--- project_sample/app/controllers/application_controller.rb
+++ project_sample/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionHelper
+   #before_action :require_login
+    def require_login
+      return if logged_in?
+      redirect_to login_path
+    end
 end
--- project_sample/config/routes.rb
+++ project_sample/config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "login" => "session#new"
+  post    "login"    => "session#create"
+  delete  "logout"   => "session#destroy"
+
+  resources :users
+
+  get "users/new"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get  "static_pages/about"
+
+  get  "static_pages/contact"
+
+  root "static_pages#home"
+
 end
