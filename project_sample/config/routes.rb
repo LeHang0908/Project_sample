2c9535c082251f642022638e2958d38df92dfaa9
LeHang0908 <lehanghd96@gmail.com>
2017-12-12 09:04:58 +0700

    Finish user signup

--- project_sample/config/routes.rb
+++ project_sample/config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+
+  resources :users
+
+  get 'users/new'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  #get  '/help',    to: 'static_pages#help'
+
+  get  'static_pages/about'
+
+  get  'static_pages/contact'
+
+  #post 'users/create'
+
+
+  #get  '/signup',  to: 'users#new'
+
+  root 'static_pages#home'
+
+  #root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
